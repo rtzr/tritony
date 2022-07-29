8ef2592f54b01cae35107a54695540fe8ffd8296
Dongwoo Arthur Kim <arthur@rtzr.ai>
2022-07-29 20:56:10 +0900

    default ASYNC_TASKS = 4

--- tritony/__init__.py
+++ tritony/__init__.py
@@ -2,7 +2,7 @@ import os
 
 from .version import __version__
 
-ASYNC_TASKS = int(os.environ.get("ASYNC_TASKS", 6))
+ASYNC_TASKS = int(os.environ.get("ASYNC_TASKS", 4))
 
 from .tools import InferenceClient
 
